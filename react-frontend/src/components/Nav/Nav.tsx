5a64c3408f70227c53d20f4eb2cfeb9f1eacd3dc
Leo Robinovitch <leorobinovitch@gmail.com>
2020-08-08 17:04:26 -0700

    add toggle to nav x

--- react-frontend/src/components/Nav/Nav.tsx
+++ react-frontend/src/components/Nav/Nav.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { toggleNav } from '../../store/actions'
 import logo from './logo.svg'
 import hamburger from './hamburger.svg'
+import exit from './exit.svg'
 
 export default function Nav () {
   const navOpen = useSelector((state: AppState) => state.navOpen)
@@ -11,17 +12,31 @@ export default function Nav () {
 
   return (
     <nav>
+      {/* logo */}
       <img className={'nav-logo'} src={logo}></img>
-      <div className={'nav-hamburger'}>
-        <button onClick={() => dispatch(toggleNav())}>
-          <img src={hamburger}></img>
-        </button>
-      </div>
+
+      {/* desktop */}
       <div className={'nav-links'}>
         <a href='/test0'>Judge</a>
         <a href='/test1'>All Colors</a>
         <a href='/test2'>Leaderboard</a>
         <a href='/test3'>What Is This</a>
+      </div>
+
+      {/* mobile */}
+      <div
+        className={'nav-hamburger'}
+        style={{ display: navOpen ? 'none' : '' }}
+      >
+        <button onClick={() => dispatch(toggleNav())}>
+          <img src={hamburger}></img>
+        </button>
+      </div>
+
+      <div className={'nav-exit'} style={{ display: navOpen ? '' : 'none' }}>
+        <button onClick={() => dispatch(toggleNav())}>
+          <img src={exit}></img>
+        </button>
       </div>
     </nav>
   )
