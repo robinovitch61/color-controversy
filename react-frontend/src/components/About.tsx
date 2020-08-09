557d65ab290f70f72e43fa3e99d8636b95016efd
Leo Robinovitch <leorobinovitch@gmail.com>
2020-08-08 23:54:08 -0700

    start about page

--- react-frontend/src/components/About.tsx
+++ react-frontend/src/components/About.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function About () {
-  return <h1>About</h1>
+  return (
+    <div className='container about'>
+      <p>
+        There is very little collective agreement on anything. Colors are
+        experienced by every person differently. This is an attempt to quantify
+        color perception. Add your perceptions with a few color judgements and
+        you too can contribute to color truth.
+      </p>
+    </div>
+  )
 }
 
 export default About
