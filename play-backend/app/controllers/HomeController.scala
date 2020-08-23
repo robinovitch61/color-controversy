2b67dffdab84bb11602c7a2cc8b90dab2225c997
Leo Robinovitch <leorobinovitch@gmail.com>
2020-08-23 12:48:30 -0700

    get janky json example working

--- play-backend/app/controllers/HomeController.scala
+++ play-backend/app/controllers/HomeController.scala
@@ -2,14 +2,46 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
+import play.api.libs.json.JsPath
 import play.api.mvc._
+import play.api.libs.json._
+
+case class Location(lat: Double, long: Double)
+
+case class Place(name: String, location: Location)
+
+object Place {
+  var list: List[Place] = {
+    List(
+      Place(
+        "Sandleford",
+        Location(51.377797, -1.318965)
+      ),
+      Place(
+        "Watership Down",
+        Location(51.235685, -1.309197)
+      )
+    )
+  }
+
+  def save(place: Place) = {
+    list = list ::: List(place)
+  }
+}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val cc: ControllerComponents) extends AbstractController(cc) {
+
+  implicit val locationWrites: Writes[Location] =
+    (JsPath \ "lat").write[Double].and((JsPath \ "long").write[Double])(unlift(Location.unapply))
+
+  implicit val placeWrites: Writes[Place] =
+    (JsPath \ "name").write[String].and((JsPath \ "location").write[Location])(unlift(Place.unapply))
 
   /**
    * Create an Action to render an HTML page.
@@ -19,6 +51,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+//    Ok(views.html.index())
+    val json = Json.toJson(Place.list)
+    Ok(json)
   }
 }
