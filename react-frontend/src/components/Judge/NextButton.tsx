024d730c41cd75e3a54ec46081d0323d705a35c7
Leo Robinovitch <leorobinovitch@gmail.com>
2020-09-14 23:18:24 -0700

    debounce next button

--- react-frontend/src/components/Judge/NextButton.tsx
+++ react-frontend/src/components/Judge/NextButton.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledJudgeButton, StyledChoiceButtonsDiv } from '../../style/style';
 
 interface NextButtonProps {
-  onClick: () => void;
+  onClick: any;
 }
 function NextButton(props: NextButtonProps) {
+  const [handleClick, setHandleClick] = useState<any>(() => {});
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHandleClick(() => props.onClick);
+    }, 400)
+  }, []);
+
   return (
     <StyledChoiceButtonsDiv>
-      <StyledJudgeButton onClick={props.onClick}>next</StyledJudgeButton>
+      <StyledJudgeButton onClick={handleClick}>next</StyledJudgeButton>
     </StyledChoiceButtonsDiv>
   );
 }
