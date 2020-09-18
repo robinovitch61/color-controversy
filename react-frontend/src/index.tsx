f95b1a2abe1b02b4c63c9d8178b2dcc74a16a700
Leo Robinovitch <leorobinovitch@gmail.com>
2020-09-17 21:14:13 -0700

    style index

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { createStore } from 'redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
-import reducer from './store/reducer'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 
-const store = createStore(reducer, devToolsEnhancer({}))
+const store = createStore(reducer, devToolsEnhancer({}));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,9 +17,9 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
