491a219f97619a23df2b6718cb1aa470e06c0d16
Leo Robinovitch <leorobinovitch@gmail.com>
2020-09-19 10:31:02 -0700

    no real progress

--- nginx/letsencrypt-nginx.sh
+++ nginx/letsencrypt-nginx.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
-apt-get update \
-    && apt-get install -y \
-    python-certbot-nginx
+# mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/www.$DOMAIN_NAME.conf
 
-mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/www.$DOMAIN_NAME.conf
-
-#service nginx start
+# service nginx start
 #nginx -g "daemon off;"
 
-certbot --nginx -d $DOMAIN_NAME -d www.$DOMAIN_NAME --email $LETSENCRYPT_EMAIL --agree-tos --redirect --test-cert --non-interactive
+certbot \
+  --nginx \
+  -d $DOMAIN_NAME \
+  -d www.$DOMAIN_NAME \
+  --email $LETSENCRYPT_EMAIL \
+  --agree-tos \
+  --redirect \
+  --non-interactive \
+  --no-eff-email \
+  --test-cert
 
-#nginx -g "daemon off;"
+# nginx -g "daemon off;"
