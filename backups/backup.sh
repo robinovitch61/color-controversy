c6ce68a2ccfe1ef94f6a6e597b49bb1ab3c41eb7
Leo Robinovitch <leorobinovitch@gmail.com>
2020-09-21 05:33:08 +0000

    fix script

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker exec postgres pg_dumpall -c -U postgres > $DIR/dump_$(date +"%Y-%m-%d_%H_%M_%S").sql
-echo $(date +"%Y-%m-%d_%H_%M_%S"), $(docker exec postgres psql -U postgres -t -c "SELECT SUM(n_first) + SUM(n_second) FROM color") >> judge_count.txt
+echo $(date +"%Y-%m-%d_%H_%M_%S"), $(docker exec postgres psql -U postgres -t -c "SELECT SUM(n_first) + SUM(n_second) FROM color") >> $DIR/judge_count.txt
